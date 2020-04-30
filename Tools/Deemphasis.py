ebb8053e7b2728a97386cebb83b4aa4b6db30ea2
Geoffrey Merck <F4FXL@users.noreply.github.com>
2020-05-01 00:02:47 +0200

    Add deamphasis python

---
+++ Tools/Deemphasis.py
@@ -0,0 +1,27 @@
+import numpy as np
+import scipy.signal as signal
+import pylab as pl
+import math
+
+fs = 48000
+tau = 750e-6
+
+w_p = 1/tau
+w_pp = math.tan (w_p / (fs * 2)) # prewarped analog freq
+
+a0 = 1
+a1 = (w_pp - 1)/(w_pp + 1)
+a2 = 0
+
+b0 = w_pp/(1 + w_pp)
+b1 = b0
+b2 = 0
+
+btaps = [b0, b1, b2]
+ataps = [a0, a1, a2]
+
+print(btaps)
+print(ataps)
+
+z,p,k = signal.tf2zpk(btaps, ataps)
+print(20*math.log10(k))
